22d373247d5bc17cbce475d18dc51c744342eff8
Livingstone Cano <livingstone23@gmail.com>
2021-08-24 00:12:37 +0200

    006 - Initial Commit

---
+++ XamarinAPP/XamarinAPP/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinAPP
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new PaginaPrincipal();
+        }
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
