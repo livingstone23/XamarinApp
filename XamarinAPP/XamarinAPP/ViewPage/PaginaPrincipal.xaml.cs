489b3175499ba0dddccce69d0d1e393edecd5d90
Livingstone Cano <livingstone23@gmail.com>
2021-08-24 21:40:16 +0200

    015 - first steps

--- XamarinAPP/XamarinAPP/ViewPage/PaginaPrincipal.xaml.cs
+++ XamarinAPP/XamarinAPP/ViewPage/PaginaPrincipal.xaml.cs
@@ -16,5 +16,22 @@ namespace XamarinAPP
         {
             InitializeComponent();
         }
+
+        private void btnVerMas_Clicked(object sender, EventArgs e)
+        {
+            string value = lblTexto.Text;
+
+            //Alerta llamado larga
+            //Application.Current.MainPage.DisplayAlert("Alerta", "Saludos", "Salir");
+
+            //Alerta llamado corta
+            //DisplayAlert("Alerta", $"Saludos de LCANO, {value}", "Salir");
+            
+            //1.Ocultar boton
+            btnVermas.IsVisible = false;
+
+            //2.Mostar todo el texto
+            lblTexto.LineBreakMode = LineBreakMode.WordWrap;
+        }
     }
 }
