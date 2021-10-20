d5cd8fc55745dc08875180de39ed22ade26e0cdc
LCANOASUS <livingstone23@gmail.com>
2021-10-20 23:17:37 +0200

    Completo seccion para registro de usuarios

--- XamarinAPP/XamarinAPP/ViewPage/RegistroUsuario.xaml.cs
+++ XamarinAPP/XamarinAPP/ViewPage/RegistroUsuario.xaml.cs
@@ -16,5 +16,19 @@ namespace XamarinAPP.ViewPage
         {
             InitializeComponent();
         }
+
+        private async void btnVolverLogin_Clicked(object sender, EventArgs e)
+        {
+            //Como si se regresase a la pantalla anterior.
+            await Navigation.PopAsync();
+        }
+
+        private void btnRegistrarUsuario_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Valores", "Nombre : " + txtnombre.Text + " Apellido paterno:" + txtapellidopaterno.Text +
+                "Apellido materno : " + txtapellidomaterno.Text + " Telefono : " + txttelefono.Text +
+                "Email " + txtemail.Text + " Fecha " + txtfechanacimiento.Date + " Descripcion " +
+                txtdescripcion.Text + " Usuario: " + txtusuario.Text + " Contraseña: " + txtcontra.Text, "Cancelar");
+        }
     }
 }
